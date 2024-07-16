5e85a7d1a760fd23e9cf0df01935361b634c7529
Rahul Vasantlal <220344548@aston.ac.uk>
2024-07-16 20:56:17 +0100

    created 16x16 grid dynamically in javascript

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.getElementById("grid-container");
+
+    for (let i = 0; i < 256; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
+});
