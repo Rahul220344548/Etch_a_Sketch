8b822e312ea72ca6a6f4cf96ad20c101945ef2a2
Rahul Vasantlal <220344548@aston.ac.uk>
2024-07-17 12:38:22 +0100

    created a hover efffect when mouse is hovered over squares

--- js/javascript.js
+++ js/javascript.js
@@ -8,5 +8,18 @@ document.addEventListener("DOMContentLoaded", function() {
         const square = document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
+
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'red';
+        });
+        
     }
 });
+
+const resizeBtn = document.querySelector(".btn");
+
+resizeBtn.addEventListener('click', () => {
+    prompt("Enter a number between 1 to 64: ");
+});
+
+
